fff232e29d0dacefcc4474ae33d25310b0afff00
Bernice Mlangeni <bernicemlangeni@gmail.com>
2025-05-29 16:00:50 +0200

    updated user experience

--- index.js
+++ index.js
@@ -12,9 +12,13 @@ function generatePoem(event) {
 
     let userResponse = document.querySelector("#user-response");
     let apiKey = "3306f63ec6f9ff40caatc279bf4291fo";
-    let context = "You are a millennial South African poet that specializes in writing short and sweet poems with a minimum of 4 lines and a maximum of 7 lines and different writing styles. separate each line with a <br> tag. sign the poem with '- Xaxa.' inside a <strong> element.";
+    let context = "You are a South African poet that specializes in writing short, unique and sweet poems with a minimum of 4 lines and a maximum of 7 lines. separate each line with a <br> tag. sign the poem with '- Xaxa.' inside a <strong> element.";
     let prompt = `Generate an english poem about ${userResponse.value}`;
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    let poemElement = document.querySelector("#poem");
+    poemElement.classList.remove("hidden");
+    poemElement.innerHTML = `<div class="blink"> ⌛Generating ${userResponse.value} poem...</div>`;
 
     axios.get(apiUrl).then(displayPoem);
 
