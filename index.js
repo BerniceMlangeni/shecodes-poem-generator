7eb184f40eab6bc7dfd61b822cef0bf87797c443
Bernice Mlangeni <bernicemlangeni@gmail.com>
2025-05-29 15:19:11 +0200

    added api integration

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-function generatePoem(event) {
-    event.preventDefault();
-
+function displayPoem(response) {
     new Typewriter("#poem", {
-        strings: ["In a meadow green and wide,"],
+        strings: response.data.answer,
         autoStart: true,
         delay: 1
     });
+}
+
+function generatePoem(event) {
+    event.preventDefault();
+
+
+    let userResponse = document.querySelector("#user-response");
+    let apiKey = "3306f63ec6f9ff40caatc279bf4291fo";
+    let context = "You are a millennial South African poet that specializes in writing short and sweet poems with a minimum of 4 lines and a maximum of 7 lines and different writing styles. separate each line with a <br> tag. sign the poem with '- Xaxa.' inside a <strong> element.";
+    let prompt = `Generate an english poem about ${userResponse.value}`;
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(displayPoem);
+
+    
 }
 
 let poemFormElement = document.querySelector("#poem-form");
